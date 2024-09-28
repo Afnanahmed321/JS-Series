e15fd2dc6376f2b378941a0f48b706eda6c270f6
Afnan <155889968+Afnanahmed321@users.noreply.github.com>
2024-09-28 14:13:33 +0000

    container config done

---
+++ Basics/datatypes.js
@@ -0,0 +1,13 @@
+/* 
+--- Primitive ---
+Some of the datatypes in js are:-
+1. number
+2. string
+3. boolean
+4. null=>standalone value - undefined aur null alag alag cheez hai
+--- null sirf khaali hai ---
+5. symbol - Will be seen in react. It is used for uniqueness
+*/
+
+console.log(typeof null) // null ka type "OBJECT" hai
+console.log(typeof undefined) // undefined ka type "UNDEFINED" hi hai
