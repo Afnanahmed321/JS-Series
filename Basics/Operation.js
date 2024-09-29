deac1bd4ca4e516bb716136c8efc0e6dfe4bb94e
Afnan <155889968+Afnanahmed321@users.noreply.github.com>
2024-09-29 12:26:02 +0000

    operations

---
+++ Basics/Operation.js
@@ -0,0 +1,15 @@
+// Basics operations
+
+/*
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3) 2 to the power 3
+console.log(2/2)
+console.log(2%2)
+*/
+
+// console.log(true)
+// console.log(+true)
+
+// Prefix and Postfix in mdn
