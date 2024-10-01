892adf1c59669ab8078e1cf7db84f4dcd5e6298f
Afnan <155889968+Afnanahmed321@users.noreply.github.com>
2024-10-01 16:05:24 +0000

    datatypes

--- Basics/03_datatypes.js
+++ Basics/03_datatypes.js
@@ -1,5 +1,5 @@
 /* 
---- Primitive ---
+--- A. Primitive ---
 Some of the datatypes in js are:-
 1. number
 2. string
@@ -7,7 +7,30 @@ Some of the datatypes in js are:-
 4. null=>standalone value - undefined aur null alag alag cheez hai
 --- null sirf khaali hai ---
 5. symbol - Will be seen in react. It is used for uniqueness
+6. bigInt
+7. undefined
 */
+
+//  B. Non-primitive(reference)
+// Arrays, objects and functions
+
 
 console.log(typeof null) // null ka type "OBJECT" hai
 console.log(typeof undefined) // undefined ka type "UNDEFINED" hi hai
+
+let userID = symbol('123')
+let anotherUserID = symbol('123')
+
+// the above 2 lines are different even their values passed in symbol are same
+
+// -------------- Non primitive
+
+// Arrays
+const myArray = ["hello","hi"]
+
+// objects
+const myObject = {
+    Name : "afnan",
+    age : 18, 
+}
+
